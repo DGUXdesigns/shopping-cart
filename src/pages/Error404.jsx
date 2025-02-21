b748f51d06c00176486dfc7c24e3204dacf15d0c
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-02-21 00:58:32 -0500

    feat(ErrorPage): add error page styling and navigation
    - Added `error-page` class to the ErrorPage component for consistent styling
    - Imported `Error404.css` to style the error page
    - Included a link to navigate back to the homepage from the error page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
+import '../css/Error404.css';
 
 function ErrorPage() {
   return (
-    <>
+    <div className="error-page">
       <h1>Page Not Found</h1>
       <p>Oops! Looks like you went the wrong way</p>
       <hr />
       <Link to={'/'}>Go Back Home</Link>
-    </>
+    </div>
   );
 }
 
