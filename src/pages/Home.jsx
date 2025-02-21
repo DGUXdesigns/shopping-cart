6f225d1a6af927646ea053ff370aa76611b38a0e
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-02-21 00:59:16 -0500

    feat(Home): update category links for shop navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import '../css/Home.css';
 
 function Home() {
@@ -7,39 +8,38 @@ function Home() {
         <h1>Categories</h1>
         <div className="hero-content">
           <div className="hero-div">
-            <img
-              src="../../public/electronics.jpg"
-              alt="Various electronic devices"
-            />
+            <img src="../../electronics.jpg" alt="Various electronic devices" />
             <div className="overlay">
               <h3 className="category-title">Electronics</h3>
               <p>
                 Discover the latest in technology, from cutting-edge smartphones
                 to high-performance laptops and accessories.
               </p>
-              <a href="#">Shop Now</a>
+              <Link to="/shop?category=electronics">Shop Now</Link>
             </div>
           </div>
+
           <div className="hero-div">
-            <img src="../../public/jewelry.jpg" alt="A watch" />
+            <img src="../../jewelry.jpg" alt="A watch" />
             <div className="overlay">
-              <h3 className="category-title">Jewelry</h3>
+              <h3 className="category-title">Jewelery</h3>
               <p>
                 Elevate your style with our exquisite collection of rings,
                 necklaces, and timepieces for every occasion.
               </p>
-              <a href="#">Shop Now</a>
+              <Link to="/shop?category=jewelery">Shop Now</Link>
             </div>
           </div>
+
           <div className="hero-div">
-            <img src="../../public/clothing.jpg" alt="A rack of T-shirts" />
+            <img src="../../clothing.jpg" alt="A rack of T-shirts" />
             <div className="overlay">
               <h3 className="category-title">Clothing</h3>
               <p>
                 Stay ahead of the trends with our stylish and comfortable
                 apparel for all seasons and occasions.
               </p>
-              <a href="#">Shop Now</a>
+              <Link to="/shop?category=clothing">Shop Now</Link>
             </div>
           </div>
         </div>
